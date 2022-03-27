5861896171e7bae34de6afbf803034bbf0158810
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-03-27 21:45:39 +0300

    init

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -8,5 +8,16 @@ interface PropsInterface {
 
 export const Card: FunctionComponent<PropsInterface> = ({ title, price }) => {
   console.log(title, price);
-  return <div className={style.wrapper}>{title}</div>;
+  return (
+    <div className={style.wrapper}>
+      <img
+        className={style.img}
+        src="https://rogovshop.ru/image/cache/catalog/05.10/211014-rogov-24575-684x900.jpg"
+      />
+      <div className={style.block}>
+        <span className={style.title}>{title}</span>
+        <span className={style.price}>{price}</span>
+      </div>
+    </div>
+  );
 };
